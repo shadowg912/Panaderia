6986fb5a6e95410d8ae70cf3ecfd7c096481885e
shadowg912 <estebandlopez912@gmial.com>
2026-02-12 07:51:59 -0400

    Actualizacion de colores y pantallas  principales

---
+++ src/main/java/model/Empresa_cliente.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Empresa_cliente {
+    private Long id;
+    private String razonSocial;
+    private String rnc;
+    private String telefono;
+    private String correo;
+}
