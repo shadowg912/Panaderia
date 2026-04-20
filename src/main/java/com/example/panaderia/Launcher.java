9ec8593452a127f313f2e40b58baef4077c392bf
sunidk114 <sunaiden7@gmail.com>
2026-04-20 12:13:09 -0400

    registrar proveedor actualizado

--- src/main/java/com/example/panaderia/Launcher.java
+++ src/main/java/com/example/panaderia/Launcher.java
@@ -2,7 +2,7 @@ package com.example.panaderia;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class n Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
